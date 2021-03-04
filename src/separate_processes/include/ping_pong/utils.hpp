c794bb07227d888ed94f61077eff0dd7252631f7
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-03-04 11:19:03 +0100

    remove utils get_timestmap() function as already defined in profiling

--- src/separate_processes/include/ping_pong/utils.hpp
+++ src/separate_processes/include/ping_pong/utils.hpp
@@ -2,20 +2,6 @@
 #include <vector>
 #include <numeric>
 
-uint64_t get_timestamp() {
-    long int ns;
-    uint64_t all;
-    time_t sec;
-    struct timespec spec;
-
-    clock_gettime(CLOCK_MONOTONIC, &spec);
-    sec = spec.tv_sec;
-    ns = spec.tv_nsec;
-
-    all = (uint64_t) sec * 1000000000UL + (uint64_t) ns;
-    
-    return all;
-}
 
 double mean(std::vector<uint64_t> samples) {
     if (samples.size() == 0)
