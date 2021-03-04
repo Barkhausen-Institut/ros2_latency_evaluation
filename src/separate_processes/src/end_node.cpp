b5c2461c20578a5c35480403aadb6543f771dd19
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-03-04 10:58:06 +0100

    rename callack

--- src/separate_processes/src/end_node.cpp
+++ src/separate_processes/src/end_node.cpp
@@ -12,13 +12,13 @@ class EndNode : public rclcpp::Node {
     public:
         EndNode() : Node("end_node") {
             subscription_ = this->create_subscription<ping_pong_interfaces::msg::PingPong>(
-                "/end_sub_topic", 10, std::bind(&EndNode::onPing, this, std::placeholders::_1)
+                "/end_sub_topic", 10, std::bind(&EndNode::onPong, this, std::placeholders::_1)
             );
         }
     
 
     private:
-        void onPing(const ping_pong_interfaces::msg::PingPong::SharedPtr msg) const {
+        void onPong(const ping_pong_interfaces::msg::PingPong::SharedPtr msg) const {
             auto now = get_timestamp();
             RCLCPP_INFO(this->get_logger(), "PING: %s", std::to_string(msg->ping_timestamp).c_str());
             RCLCPP_INFO(this->get_logger(), "PONG: %s", std::to_string(msg->pong_timestamp).c_str());
