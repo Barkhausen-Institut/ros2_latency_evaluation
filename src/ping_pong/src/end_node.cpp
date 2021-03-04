6ec7b55668678580c38b5d598c23dd26b91c3136
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-03-04 09:14:48 +0100

    use int64 as msg type

--- src/ping_pong/src/end_node.cpp
+++ src/ping_pong/src/end_node.cpp
@@ -1,24 +1,24 @@
 #include <chrono>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/int64.hpp"
 
 using namespace std::chrono_literals;
 class EndNode : public rclcpp::Node {
     public:
         EndNode() : Node("end_node") {
-            publisher_ = this->create_publisher<std_msgs::msg::String>("/first_sub_topic", 10);
-            subscription_ = this->create_subscription<std_msgs::msg::String>(
+            publisher_ = this->create_publisher<std_msgs::msg::Int64>("/first_sub_topic", 10);
+            subscription_ = this->create_subscription<std_msgs::msg::Int64>(
                 "/first_pub_topic", 10, std::bind(&EndNode::onPing, this, std::placeholders::_1)
             );
         }
     
     private:
-        void onPing(const std_msgs::msg::String::SharedPtr msg) const {
+        void onPing(const std_msgs::msg::Int64::SharedPtr msg) const {
             publisher_->publish(*msg);
         }
-        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+        rclcpp::Publisher<std_msgs::msg::Int64>::SharedPtr publisher_;
+        rclcpp::Subscription<std_msgs::msg::Int64>::SharedPtr subscription_;
 };
 
 int main(int argc, char* argv[]) {
--- src/ping_pong/src/start_node.cpp
+++ src/ping_pong/src/start_node.cpp
@@ -1,34 +1,35 @@
 #include <chrono>
+#include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/int64.hpp"
 
 using namespace std::chrono_literals;
 class StartNode : public rclcpp::Node {
     public:
         StartNode() : Node("start_node") {
-            publisher_ = this->create_publisher<std_msgs::msg::String>("/start_pub_topic", 10);
+            publisher_ = this->create_publisher<std_msgs::msg::Int64>("/start_pub_topic", 10);
             timer_ = this->create_wall_timer(
                 500ms, std::bind(&StartNode::timer_callback, this));
-            subscription_ = this->create_subscription<std_msgs::msg::String>(
+            subscription_ = this->create_subscription<std_msgs::msg::Int64>(
                 "/start_sub_topic", 10, std::bind(&StartNode::onPong, this, std::placeholders::_1)
             );
         }
     
     private:
         void timer_callback() {
-            auto msg = std_msgs::msg::String();
-            msg.data = "hi";
-            RCLCPP_INFO(this->get_logger(), "Publishing %s", msg.data.c_str());
+            auto msg = std_msgs::msg::Int64();
+            msg.data = 1;
+            RCLCPP_INFO(this->get_logger(), "Publishing %s", std::to_string(msg.data).c_str());
             publisher_->publish(msg);
         }
 
-        void onPong(const std_msgs::msg::String::SharedPtr msg) const {
-            RCLCPP_INFO(this->get_logger(), "I heard %s", msg->data.c_str());
+        void onPong(const std_msgs::msg::Int64::SharedPtr msg) const {
+            RCLCPP_INFO(this->get_logger(), "I heard %s", std::to_string(msg->data).c_str());
         }
         rclcpp::TimerBase::SharedPtr timer_;
-        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+        rclcpp::Publisher<std_msgs::msg::Int64>::SharedPtr publisher_;
+        rclcpp::Subscription<std_msgs::msg::Int64>::SharedPtr subscription_;
 };
 
 int main(int argc, char* argv[]) {
