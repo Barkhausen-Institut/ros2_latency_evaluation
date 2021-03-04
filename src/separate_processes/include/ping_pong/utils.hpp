f12bf8fd3268aa79819ded17bbd685d0235c15ea
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-03-04 10:38:27 +0100

    include online calculation of mean and variance

--- src/separate_processes/include/ping_pong/utils.hpp
+++ src/separate_processes/include/ping_pong/utils.hpp
@@ -1,4 +1,6 @@
 #include <time.h>
+#include <vector>
+#include <numeric>
 
 uint64_t get_timestamp() {
     long int ns;
@@ -13,4 +15,24 @@ uint64_t get_timestamp() {
     all = (uint64_t) sec * 1000000000UL + (uint64_t) ns;
     
     return all;
+}
+
+double mean(std::vector<uint64_t> samples) {
+    if (samples.size() == 0)
+        return 0;
+
+    double sum = std::accumulate(samples.begin(), samples.end(), 0.0);
+    return sum / samples.size();
+}
+
+double variance(std::vector<uint64_t> samples) {
+    if (samples.size() <= 1)
+        return 0;
+
+    double samplesMean = mean(samples);
+    double sqSum = 0;
+    for (int i = 0; i < samples.size(); i++) {
+        sqSum += (samples.at(i) * samples.at(i));
+    }
+    return (sqSum/(samples.size()-1) - samplesMean * samplesMean);
 }
