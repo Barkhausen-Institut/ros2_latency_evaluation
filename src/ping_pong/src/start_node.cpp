56b846ccfaeed68191ac2337dd8eb93ef70cbeb8
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-03-04 10:10:58 +0100

    calculate time difference in msg and store it in vector

--- src/ping_pong/src/start_node.cpp
+++ src/ping_pong/src/start_node.cpp
@@ -1,9 +1,11 @@
 #include <chrono>
 #include <string>
-#include "utils.hpp"
+#include <vector>
 
 #include "rclcpp/rclcpp.hpp"
 #include "ping_pong_interfaces/msg/ping_pong.hpp"
+
+#include "utils.hpp"
 
 using namespace std::chrono_literals;
 class StartNode : public rclcpp::Node {
@@ -11,7 +13,7 @@ class StartNode : public rclcpp::Node {
         StartNode() : Node("start_node") {
             publisher_ = this->create_publisher<ping_pong_interfaces::msg::PingPong>("/start_pub_topic", 10);
             timer_ = this->create_wall_timer(
-                500ms, std::bind(&StartNode::timer_callback, this));
+                100ms, std::bind(&StartNode::timer_callback, this));
             subscription_ = this->create_subscription<ping_pong_interfaces::msg::PingPong>(
                 "/start_sub_topic", 10, std::bind(&StartNode::onPong, this, std::placeholders::_1)
             );
@@ -25,12 +27,16 @@ class StartNode : public rclcpp::Node {
             publisher_->publish(msg);
         }
 
-        void onPong(const ping_pong_interfaces::msg::PingPong::SharedPtr msg) const {
-            RCLCPP_INFO(this->get_logger(), "I heard %s", std::to_string(msg->pong_timestamp).c_str());
+        void onPong(const ping_pong_interfaces::msg::PingPong::SharedPtr msg) {
+            auto pong_received_timestamp = get_timestamp();
+            uint64_t msg_delay = pong_received_timestamp - msg->ping_timestamp;
+            delays_in_ms_.push_back(msg_delay/1000);
+            RCLCPP_INFO(this->get_logger(), "Msg Delay is %s", std::to_string(msg_delay/1000).c_str());
         }
         rclcpp::TimerBase::SharedPtr timer_;
         rclcpp::Publisher<ping_pong_interfaces::msg::PingPong>::SharedPtr publisher_;
         rclcpp::Subscription<ping_pong_interfaces::msg::PingPong>::SharedPtr subscription_;
+        std::vector<uint64_t> delays_in_ms_;
 };
 
 int main(int argc, char* argv[]) {
