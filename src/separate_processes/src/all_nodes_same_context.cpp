8a78fe5004af20fb7bb881687a8bab6b53956623
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-03-05 16:23:10 +0100

    start nodes in same process with same context

---
+++ src/separate_processes/src/all_nodes_same_context.cpp
@@ -0,0 +1,31 @@
+#include "node_definitions.hpp"
+#include <iostream>
+
+#include <thread>
+
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+
+    auto startNode = std::make_shared<StartNode>();
+    auto interNode = std::make_shared<IntermediateNode>();
+    auto endNode = std::make_shared<EndNode>();
+
+    auto exStartNode = std::make_shared<rclcpp::executors::StaticSingleThreadedExecutor>();
+    auto exInterNode = std::make_shared<rclcpp::executors::StaticSingleThreadedExecutor>();
+    auto exEndNode = std::make_shared<rclcpp::executors::StaticSingleThreadedExecutor>();
+
+    exStartNode->add_node(startNode);
+    exInterNode->add_node(interNode);
+    exEndNode->add_node(endNode);
+
+    std::thread threadStartNode([=] {exStartNode->spin(); } );
+    std::thread threadIntermediateNode([=] {exInterNode->spin(); } );
+    std::thread threadEndNode([=] {exEndNode->spin(); } );
+
+    threadStartNode.join();
+    threadIntermediateNode.join();
+    threadEndNode.join();
+
+    rclcpp::shutdown();
+    return 0;
+}
