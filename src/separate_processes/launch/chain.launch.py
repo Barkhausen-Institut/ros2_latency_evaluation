60e78dbfe16e7af2ed731940c66cdb06147c46d7
Maximilian Matthe <maxi.matthe@googlemail.com>
2021-03-08 12:01:09 +0100

    remove dead code

--- src/separate_processes/launch/chain.launch.py
+++ src/separate_processes/launch/chain.launch.py
@@ -24,22 +24,7 @@ def generate_launch_description():
 
     nodes.append(Node(package=PKG, executable='end_node',
                       remappings=[("/end_sub_topic", f"/step_{intNodes}")]))
-        #Node(
-            #package='turtlesim',
-            #namespace='turtlesim2',
-            #executable='turtlesim_node',
-            #name='sim'
-        #),
-        #Node(
-            #package='turtlesim',
-            #executable='mimic',
-            #name='mimic',
-            #remappings=[
-                #('/input/pose', '/turtlesim1/turtle1/pose'),
-                #('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-            #]
-        #)
-    # ]
+
     return LaunchDescription(nodes)
 
 print (sys.argv)
