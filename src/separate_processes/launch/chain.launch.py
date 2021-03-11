3ceddbf6a073f92c2d4bddde8b04560f9aa55062
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-03-11 14:42:45 +0100

    pass arguments to each node

--- src/separate_processes/launch/chain.launch.py
+++ src/separate_processes/launch/chain.launch.py
@@ -12,22 +12,33 @@ def generate_launch_description():
     intNodes = int(os.environ.get("INT_NODES", "1"))
     pubFrequency = os.environ.get("PUB_FREQUENCY", "1")
     msgSize = os.environ.get("MSG_SIZE", "100b")
+
+    nodeArgsList = ['-f', pubFrequency, '-n', str(intNodes), '-m', msgSize]
     nodes = []
     nodes.append(Node(
                     package=PKG,
                     executable='start_node',
                     remappings=[("/start_pub_topic", "/step_0")],
-                    arguments=['-f', pubFrequency, '-n', str(intNodes), '-m', msgSize],
-                    name="head"))
+                    arguments=nodeArgsList,
+                    name="start_node"))
     for i in range(intNodes):
         remappings = []
         remappings.append(("/start_pub_topic", f"/step_{i}"))
         remappings.append(("/end_sub_topic", f"/step_{i+1}"))
         print(remappings)
-        nodes.append(Node(package=PKG, executable='intermediate_node', remappings=remappings))
+        nodes.append(Node(
+                    package=PKG,
+                    executable='intermediate_node',
+                    remappings=remappings,
+                    arguments=nodeArgsList,
+                    name=f"intermediate_node_{i}"))
 
-    nodes.append(Node(package=PKG, executable='end_node',
-                      remappings=[("/end_sub_topic", f"/step_{intNodes}")]))
+    nodes.append(Node(
+                    package=PKG,
+                    executable='end_node',
+                    remappings=[("/end_sub_topic", f"/step_{intNodes}")],
+                    arguments=nodeArgsList,
+                    name="end_node"))
 
     return LaunchDescription(nodes)
 
