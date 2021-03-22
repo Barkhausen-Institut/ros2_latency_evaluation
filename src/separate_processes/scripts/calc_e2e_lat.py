96f778815baa69caeefff1bf297fe109ec34118a
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-03-22 13:37:40 +0100

    sort csv files according to node index

--- src/separate_processes/scripts/calc_e2e_lat.py
+++ src/separate_processes/scripts/calc_e2e_lat.py
@@ -1,10 +1,19 @@
 import os
 import argparse
 from glob import glob
+from typing import List
 
-def getNodeIndexFromDumpedCsvFileName(filename: str):
+def getNodeIndexFromDumpedCsvFileName(filename: str) -> int:
     nodeIdx = int(filename.split('-')[0])
     return nodeIdx
+
+def sortCsvFiles(csvFiles: List[str]):
+    unsortedFiles = {}
+    for f in csvFiles:
+        unsortedFiles[getNodeIndexFromDumpedCsvFileName(f)] = f
+
+    sortedFiles = {k: unsortedFiles[k] for k in sorted(unsortedFiles)}
+    return sortedFiles
 
 parser = argparse.ArgumentParser()
 parser.add_argument('directory', type=str, help='relative path to directory containing dumped csvs.')
@@ -14,6 +23,7 @@ if not os.path.exists(args.directory):
     raise FileNotFoundError(f"Directory {args.directory} does not exist.")
 
 dumpedCsvsPerRun = [os.path.basename(f) for f in glob(f"{args.directory}/*")]
+sortedCsvs = sortCsvFiles(dumpedCsvsPerRun)
 
 print(f"Processing directory: {args.directory}")
 for dumpedCsv in dumpedCsvsPerRun:
