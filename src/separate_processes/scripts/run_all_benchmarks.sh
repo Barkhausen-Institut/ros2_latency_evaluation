ea208aa0617df2b6c9bf7b7323b6763a6a36789e
Maximilian Matthe <maxi.matthe@googlemail.com>
2021-03-24 11:35:36 +0100

    add initial script to test benchmarking all settings

---
+++ src/separate_processes/scripts/run_all_benchmarks.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#############################
+# change variables here
+
+DDS_BACKENDS='rmw_connextdds rmw_fastrtps_cpp rmw_cyclonedds_cpp'
+#DDS_BACKENDS='rmw_fastrtps_cpp'
+
+INT_NODES_SET=$(seq 1 2 9)
+INT_NODES_SET="5"
+
+F_PUBLISHER_SET='1 10 20 30 40 50 60 70 80 90 100'
+F_PUBLISHER_SET="20"
+
+MSG_SIZE_SET='128b 1kb 10kb 100kb 500kb'
+MSG_SIZE_SET='128b'
+
+QOS_RELIABILITY_SET='best-effort reliable'
+QOS_RELIABILITY_SET='best-effort'
+
+TEST_DURATION=1
+
+####################################################
+## change below only if you know what you are doing
+####################################################
+BASE="ros2 launch ../launch/chain.launch.py "
+
+for QOS_RELIABILITY in $QOS_RELIABILITY_SET
+do
+    for MSG_SIZE in $MSG_SIZE_SET
+    do
+        for F_PUBLISHER in $F_PUBLISHER_SET
+        do
+            for BACKEND in $DDS_BACKENDS
+            do
+		for INT_NODES in $INT_NODES_SET
+		do
+		    timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+		    echo "[$timestamp]:"
+		    command="INT_NODES=$INT_NODES \
+                             PUB_FREQUENCY=$F_PUBLISHER \
+                             MSG_SIZE=$MSG_SIZE \
+                             QOS=$QOS_RELIABILITY \
+                             DURATION=$TEST_DURATION \
+                             RMW_IMPLEMENTATION=$BACKEND \
+                             $BASE"
+		    /bin/bash -c "$command"
+		    echo "[$timestamp]: $command" >> log.txt
+		done
+	    done
+        done
+    done
+done
