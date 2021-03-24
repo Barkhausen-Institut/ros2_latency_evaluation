ad3201a64f0df27479fdd7eefacc312845ef36d2
Maximilian Matthe <maxi.matthe@googlemail.com>
2021-03-24 11:43:03 +0100

    add a test param set for quick feedback

--- src/separate_processes/scripts/run_all_benchmarks.sh
+++ src/separate_processes/scripts/run_all_benchmarks.sh
@@ -2,23 +2,26 @@
 
 #############################
 # change variables here
+TEST=1  # set to 1, to choose the test param set.
 
-DDS_BACKENDS='rmw_connextdds rmw_fastrtps_cpp rmw_cyclonedds_cpp'
-#DDS_BACKENDS='rmw_fastrtps_cpp'
+if [ "1" -eq $TEST ]; then
+    DDS_BACKENDS='rmw_fastrtps_cpp'
+    INT_NODES_SET="5"
+    F_PUBLISHER_SET="20"
+    MSG_SIZE_SET='128b'
+    QOS_RELIABILITY_SET='best-effort'
 
-INT_NODES_SET=$(seq 1 2 9)
-INT_NODES_SET="5"
+    TEST_DURATION=1
+else
+    DDS_BACKENDS='rmw_connextdds rmw_fastrtps_cpp rmw_cyclonedds_cpp'
+    INT_NODES_SET=$(seq 1 2 9)
+    F_PUBLISHER_SET='1 10 20 30 40 50 60 70 80 90 100'
+    MSG_SIZE_SET='128b 1kb 10kb 100kb 500kb'
+    QOS_RELIABILITY_SET='best-effort reliable'
 
-F_PUBLISHER_SET='1 10 20 30 40 50 60 70 80 90 100'
-F_PUBLISHER_SET="20"
+    TEST_DURATION=60
+fi
 
-MSG_SIZE_SET='128b 1kb 10kb 100kb 500kb'
-MSG_SIZE_SET='128b'
-
-QOS_RELIABILITY_SET='best-effort reliable'
-QOS_RELIABILITY_SET='best-effort'
-
-TEST_DURATION=1
 
 ####################################################
 ## change below only if you know what you are doing
