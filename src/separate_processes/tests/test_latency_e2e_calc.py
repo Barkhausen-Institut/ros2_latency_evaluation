add test for correct e2e latency calculation

src/separate_processes/tests/test_latency_e2e_calc.py
@@ -14,24 +14,24 @@ class TestE2eLat(unittest.TestCase):
     def _createTestFiles(self):
         NO_PROFILING_TIMESTAMPS = 14
         HEADERS = ["header_timestamp"] + [f"prof_{i}" for i in range(NO_PROFILING_TIMESTAMPS)] + ["callback_timestamp"]
-        T_MSG_SENT = [0, 1e3, 2e3]
+        self.T_MSG_SENT = [0, 1e3, 2e3]
         DELAY_INTER_NODE = 5
 
-        t_secondNode = [dict.fromkeys(HEADERS) for i in range(len(T_MSG_SENT))]
-        t_thirdNode = [dict.fromkeys(HEADERS) for i in range(len(T_MSG_SENT))]
+        self.t_secondNode = [dict.fromkeys(HEADERS) for i in range(len(self.T_MSG_SENT))]
+        self.t_thirdNode = [dict.fromkeys(HEADERS) for i in range(len(self.T_MSG_SENT))]
 
-        for msgCount, tMsgSent in enumerate(T_MSG_SENT):
+        for msgCount, tMsgSent in enumerate(self.T_MSG_SENT):
             # fill up second node profiling
-            t_secondNode[msgCount]["header_timestamp"] = tMsgSent
+            self.t_secondNode[msgCount]["header_timestamp"] = tMsgSent
             for i in range(NO_PROFILING_TIMESTAMPS):
-                t_secondNode[msgCount][f"prof_{i}"] = tMsgSent + (i + 1) * 10
-            t_secondNode[msgCount]["callback_timestamp"] = t_secondNode[msgCount]["prof_13"] + 20
+                self.t_secondNode[msgCount][f"prof_{i}"] = tMsgSent + (i + 1) * 10
+            self.t_secondNode[msgCount]["callback_timestamp"] = self.t_secondNode[msgCount]["prof_13"] + 20
 
             # fill up third node profiling
-            t_thirdNode[msgCount]["header_timestamp"] = tMsgSent
+            self.t_thirdNode[msgCount]["header_timestamp"] = tMsgSent
             for i in range(NO_PROFILING_TIMESTAMPS):
-                t_thirdNode[msgCount][f"prof_{i}"] = tMsgSent + (i + 1) * 10 + DELAY_INTER_NODE
-            t_thirdNode[msgCount]["callback_timestamp"] = t_thirdNode[msgCount]["prof_13"] + 20
+                self.t_thirdNode[msgCount][f"prof_{i}"] = tMsgSent + (i + 1) * 10 + DELAY_INTER_NODE
+            self.t_thirdNode[msgCount]["callback_timestamp"] = self.t_thirdNode[msgCount]["prof_13"] + 20
 
         with open('0-3.csv', 'w') as f:
             writer = csv.DictWriter(f, fieldnames=HEADERS)
@@ -40,12 +40,12 @@ class TestE2eLat(unittest.TestCase):
         with open('1-3.csv', 'w') as f:
             writer = csv.DictWriter(f, fieldnames=HEADERS)
             writer.writeheader()
-            writer.writerows(t_secondNode)
+            writer.writerows(self.t_secondNode)
 
         with open('2-3.csv', 'w') as f:
             writer = csv.DictWriter(f, fieldnames=HEADERS)
             writer.writeheader()
-            writer.writerows(t_thirdNode)
+            writer.writerows(self.t_thirdNode)
     
     def tearDown(self) -> None:
         os.remove('0-3.csv')
@@ -53,5 +53,11 @@ class TestE2eLat(unittest.TestCase):
         os.remove('2-3.csv')
 
     def test_correctEToELatency(self) -> None:
-        pass
-        
+        e2eLatencies = np.zeros(len(self.T_MSG_SENT))
+        for i in range(len(self.T_MSG_SENT)):
+            e2eLatencies[i] = self.t_thirdNode[i]["callback_timestamp"] - self.t_secondNode[i]["header_timestamp"]
+
+        np.testing.assert_array_almost_equal(
+            self.calculatedLatencies["e2e"],
+            e2eLatencies
+        )