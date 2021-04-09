c0733071105b64dc406568193959434835879208
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-04-09 08:17:28 +0200

    add res directory as parameter + check if parent directory exists

--- src/separate_processes/scripts/collect_lat_csv.py
+++ src/separate_processes/scripts/collect_lat_csv.py
@@ -1,9 +1,16 @@
 import argparse
+import os
+from glob import glob
 
+
+def processDirectory(args) -> None:
+    if not os.path.exists(args.directory):
+        raise FileNotFoundError(f"Directory {parentDir} does not exist.")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--directory', type=str, help='relative path to parent directory containing dumped csvs.')
+    parser.add_argument('--directory', type=str, default=".", help='relative path to parent directory containing dumped csvs.')
+    parser.add_argument('--res-dir', type=str, default="results_paper", help="Directory to save post-processed data to.")
     parser.add_argument('--nodes', nargs="+", default=[3, 23, 1], help="""Number of nodes to process. Pass it as follows:
                                                                           <start_nodes> <end_nodes> <step_size>.""")
     parser.add_argument('--rmw', type=str, default="fastrtps", help="Choose RMW. Allowed values: cyclone, fastrtps, connext")
@@ -12,3 +19,8 @@ if __name__ == '__main__':
     parser.add_argument('--reliability', type=str, default="best_effort", help="Reliability, best_effort or reliable")
     args = parser.parse_args()
 
+    for resultsDir in glob(os.path.join(args.directory, "*")):
+        print(f"Parsing directory: {resultsDir}")
+        processDirectory(args)
+
+        break
