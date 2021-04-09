73ad71713f7d487994b60c2171286a79610ea244
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-04-09 08:14:03 +0200

    add realibility

--- src/separate_processes/scripts/collect_lat_csv.py
+++ src/separate_processes/scripts/collect_lat_csv.py
@@ -6,8 +6,9 @@ if __name__ == '__main__':
     parser.add_argument('--directory', type=str, help='relative path to parent directory containing dumped csvs.')
     parser.add_argument('--nodes', nargs="+", default=[3, 23, 1], help="""Number of nodes to process. Pass it as follows:
                                                                           <start_nodes> <end_nodes> <step_size>.""")
-    parser.add_argument('--rmw', type=str, help="Choose RMW. Allowed values: cyclone, fastrtps, connext")
-    parser.add_argument('--f', type=int, help="Publisher Frequency in Hz.")
-    parsre.add_argument('--msg-size', type=str, help="Size of the message.")
+    parser.add_argument('--rmw', type=str, default="fastrtps", help="Choose RMW. Allowed values: cyclone, fastrtps, connext")
+    parser.add_argument('--f', type=int, default=1, help="Publisher Frequency in Hz.")
+    parser.add_argument('--msg-size', type=str, default="128b", help="Size of the message.")
+    parser.add_argument('--reliability', type=str, default="best_effort", help="Reliability, best_effort or reliable")
     args = parser.parse_args()
 
