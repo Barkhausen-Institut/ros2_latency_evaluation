bugfix in calculate agg pkg errrors calculation

src/separate_processes/scripts/analyze_package_drops.py
@@ -19,7 +19,7 @@ def calculateCdf(lat: np.array) -> Tuple[np.array, np.array]:
 
 def calculateAggPkgErrors(pkgs: np.array) -> np.array:
     errors = np.diff(pkgs) - 1
-    aggErrors = np.cumsum(errors)
+    aggErrors = np.cumsum(errors) + pkgs[0]
     aggErrors = np.append(np.array([0]), aggErrors)
     return aggErrors
 